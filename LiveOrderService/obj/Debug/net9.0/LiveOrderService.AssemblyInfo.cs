666ea0349c4c060eae1f44d29bb4d6eb6da4e3d4
Demetrio M. Junior <71155627+demetriomjr@users.noreply.github.com>
2025-03-10 06:40:15 -0300

    Update assembly information and resolve package dependency warnings

--- LiveOrderService/obj/Debug/net9.0/LiveOrderService.AssemblyInfo.cs
+++ LiveOrderService/obj/Debug/net9.0/LiveOrderService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LiveOrderService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f525f9250b96839f294b3898385641ebe7bf11ec")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b42bccf0f639ae6de7e9ad2554e15103d47027e3")]
 [assembly: System.Reflection.AssemblyProductAttribute("LiveOrderService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LiveOrderService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
